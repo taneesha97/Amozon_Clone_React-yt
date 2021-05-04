d5beaff176f3de64543b46835ff4feaccccc2782
taneesha97 <68469720+taneesha97@users.noreply.github.com>
2021-05-04 17:45:42 +0530

    header part done partialy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
       //BEM
     <div className="App">
       <h1>Hello Programmers</h1>
-        {/* Header */}
+        <Header />
         {/* Home */}
     </div>
   );
